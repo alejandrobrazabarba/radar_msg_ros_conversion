08f560aebe3a2561be3f56aa1332985c7675b974
Alejandro Braza Barba <alejandrobrazabarba@gmail.com>
2018-10-19 17:13:27 +0200

    Versión asíncrona
    Modificado el nombre el script de prueba.
    Recepción de los mensajes UDP de manera asíncrona, de manera que no hay bloqueo esperando su recepción, lo que causaba problemas a la hora de cerrar el proceso. Eliminados los archivos de ejemplo que ya no son necesarios.

--- include/radar_msg_ros_conversion/sehirus_conversion.h
+++ include/radar_msg_ros_conversion/sehirus_conversion.h
@@ -3,13 +3,15 @@
 
 #include <iostream>
 #include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/asio.hpp>
+#include <boost/chrono.hpp>
 #include <pthread.h>
 #include "ros/ros.h"
 #include "radar_msg_ros_conversion/SehirusHeartbeat.h"
 
 typedef boost::asio::ip::udp boostUdp;
-typedef boost::asio::detail::socket_option::integer<SOL_SOCKET, SO_RCVTIMEO> rcv_timeout_option;
 
 class SehirusConversion {
 public:
@@ -17,8 +19,16 @@ public:
 	~SehirusConversion();
 	void main();
 private:
+	void udp_handle_receive(const boost::system::error_code& error,
+							std::size_t num_bytes);
+
 	ros::NodeHandle _nodeHandle;
 	ros::Publisher	_heartBeatPublisher;
+	boost::array<uint8_t, 1> _send_buf;
+	boost::array<uint8_t, 72> _recv_buf;
+	boost::asio::io_service	_io_service;
+	boostUdp::endpoint _server_endpoint;
+	boostUdp::socket _socket;
 };
 
 
--- src/sehirus_conversion.cpp
+++ src/sehirus_conversion.cpp
@@ -11,7 +11,8 @@ heartBeatMsg.command_listen
 * SehirusConversion constructor
 */
 SehirusConversion::SehirusConversion():
-_nodeHandle()
+_nodeHandle(),
+_socket(_io_service)
 {
 }
 
@@ -23,81 +24,96 @@ SehirusConversion::~SehirusConversion()
 
 }
 
+void SehirusConversion::udp_handle_receive(const boost::system::error_code& error,
+										   std::size_t num_bytes)
+{
+	if (!error)
+	{
+		radar_msg_ros_conversion::SehirusHeartbeat heartBeatMsg;
+
+		// Probemos primero únicamente con los siguientes campos del mensaje heartbeat
+		heartBeatMsg.msgIdentifier = (uint32_t)_recv_buf[3] << 24  |
+      				  				 (uint32_t)_recv_buf[2] << 16 |
+      				  				 (uint32_t)_recv_buf[1] << 8  |
+      				  				 (uint32_t)_recv_buf[0];
+
+		//std::cout.write(_recv_buf.data(), len);
+		//std::cout << "Something is received" << std::endl;
+		// Si recibimos los datos en formato big-endian
+		/*
+		num = (uint32_t)buffer[0] << 24 |
+      		  (uint32_t)buffer[1] << 16 |
+      	  	(uint32_t)buffer[2] << 8  |
+      	  	(uint32_t)buffer[3];
+    	// Si recibimos los datos en formato little-endian
+		num = (uint32_t)buffer[3] << 24 |
+      		  (uint32_t)buffer[2] << 16 |
+      		  (uint32_t)buffer[1] << 8  |
+      	  	  (uint32_t)buffer[0];
+		*/
+
+    	char serverName[16];
+    	for(int i=0; i<=16; i++) {
+      		serverName[i] = _recv_buf[4+i]; 
+    	}
+		heartBeatMsg.serverName = std::string(serverName);
+		heartBeatMsg.commandPort = (uint16_t) _recv_buf[21] << 8 |
+								   (uint16_t) _recv_buf[20];
+		heartBeatMsg.commandConnected = (uint8_t) _recv_buf[22];
+		heartBeatMsg.commandListen = (uint8_t) _recv_buf[23];
+		_heartBeatPublisher.publish(heartBeatMsg);
+
+		_socket.async_receive_from(
+			boost::asio::buffer(_recv_buf), _server_endpoint,
+			boost::bind(&SehirusConversion::udp_handle_receive, this,
+			boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred));
+	}
+}
+
 void SehirusConversion::main()
 {
 	_heartBeatPublisher = _nodeHandle.advertise<radar_msg_ros_conversion::SehirusHeartbeat>("heartbeat", 10);
 	// Aquí debo añadir más publicadores para el resto de mensajes
-	radar_msg_ros_conversion::SehirusHeartbeat heartBeatMsg;
 	ros::Duration(5).sleep();
 	//*************************
-	while(ros::ok())
+
+	try
 	{
-		try
-		{
-			boost::asio::io_service io_service;
+		boostUdp::resolver resolver(_io_service);
+		// A la consulta se le pasan los siguientes argumentos
+		// 1º Protocolo a usar, en este caso UDP/IPv4
+		// 2º El nombre del servidor (FQDN)
+		// 3º El servicio (o puerto) al que se pretende acceder (ver Lista de puertos TCP y UDP en wikipedia)
+		boostUdp::resolver::query query(boostUdp::v4(), "localhost", "1234");
+		_server_endpoint = *resolver.resolve(query);
 
-			boostUdp::resolver resolver(io_service);
-			// A la consulta se le pasan los siguientes argumentos
-			// 1º Protocolo a usar, en este caso UDP/IPv4
-			// 2º El nombre del servidor (FQDN)
-			// 3º El servicio (o puerto) al que se pretende acceder (ver Lista de puertos TCP y UDP en wikipedia)
-			boostUdp::resolver::query query(boostUdp::v4(), "localhost", "1234");
-			boostUdp::endpoint server_endpoint = *resolver.resolve(query);
+		// Imprimir la dirección IP del endpoint
+		std::cout << "dirección IP del servidor " << _server_endpoint.address() << std::endl;
 
-			// Imprimir la dirección IP del endpoint
-			std::cout << "dirección IP del servidor " << server_endpoint.address() << std::endl;
+		//boostUdp::socket socket(_io_service);
+		//_socket = boostUdp::socket(_io_service);
+		_socket.open(boostUdp::v4());
 
-			boostUdp::socket socket(io_service);
-			socket.open(boostUdp::v4());
+		_send_buf[0] = 42;
+		_socket.send_to(boost::asio::buffer(_send_buf), _server_endpoint);
 
-			boost::array<uint8_t, 1> send_buf  = {{ 42 }};
-			socket.send_to(boost::asio::buffer(send_buf), server_endpoint);
+		_socket.async_receive_from(
+			boost::asio::buffer(_recv_buf), _server_endpoint,
+			boost::bind(&SehirusConversion::udp_handle_receive, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
 
-			boost::array<uint8_t, 72> recv_buf;
-			//boostUdp::endpoint client_endpoint;
-			std::cout << "Trying to receive something" << std::endl;
-			size_t len = socket.receive_from(
-		    	boost::asio::buffer(recv_buf), server_endpoint);
-
-			//std::cout.write(recv_buf.data(), len);
-			//std::cout << "Something is received" << std::endl;
-
-			// Si recibimos los datos en formato big-endian
-			/*
-			num = (uint32_t)buffer[0] << 24 |
-      			  (uint32_t)buffer[1] << 16 |
-      			  (uint32_t)buffer[2] << 8  |
-      			  (uint32_t)buffer[3];
-      		// Si recibimos los datos en formato little-endian
-			num = (uint32_t)buffer[3] << 24 |
-      			  (uint32_t)buffer[2] << 16 |
-      			  (uint32_t)buffer[1] << 8  |
-      			  (uint32_t)buffer[0];
-			*/
-
-			// Probemos primero únicamente con los siguientes campos del mensaje heartbeat
-			heartBeatMsg.msgIdentifier = (uint32_t)recv_buf[3] << 24  |
-      			  				  	  (uint32_t)recv_buf[2] << 16 |
-      			  				  	  (uint32_t)recv_buf[1] << 8  |
-      			  				  	  (uint32_t)recv_buf[0];
-
-      		char serverName[16];
-      		for(int i=0; i<=16; i++) {
-      			serverName[i] = recv_buf[4+i]; 
-      		}
-			heartBeatMsg.serverName = std::string(serverName);
-			heartBeatMsg.commandPort = (uint16_t) recv_buf[21] << 8 |
-										(uint16_t) recv_buf[20];
-			heartBeatMsg.commandConnected = (uint8_t) recv_buf[22];
-			heartBeatMsg.commandListen = (uint8_t) recv_buf[23];
-			_heartBeatPublisher.publish(heartBeatMsg);
-
-		}
-		catch (std::exception& e)
-		{
-			//std::cerr << e.what() << std::endl;
-			ROS_WARN_STREAM("Exception related with UDP socket: " << e.what());
-		}
+	}
+	catch (std::exception& e)
+	{
+		//std::cerr << e.what() << std::endl;
+		ROS_WARN_STREAM("Exception related with UDP socket: " << e.what());
+	}
+	while (ros::ok())
+	{
+		_io_service.poll_one();
+		ros::Duration(1).sleep();
 	}
 }
 
